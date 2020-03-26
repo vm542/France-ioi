a1d6a1f3c258673af4917f4547a261c3be72a0a4
maissouradze <nicolas.maissouradze@gmail.com>
2020-03-26 23:46:05 +0100

    Ski de fond I

--- Niveau 6/6 – Flots et couplages/4) Ski de fond I/main.cpp
+++ Niveau 6/6 – Flots et couplages/4) Ski de fond I/main.cpp
@@ -10,26 +10,21 @@ using namespace std;
 
 int nbJonctions, nbTuyaux, nbBallades;
 int jonctions[MAX_NB_JONCTIONS + 1][MAX_NB_JONCTIONS + 1];
-int maxFlot = 0;
-int visite[MAX_NB_JONCTIONS + 1];
-
+int visite[MAX_NB_JONCTIONS + 1], nbChemins = 0;
 vector<pair<int,int>>   chemin;
+vector<int> chemins[MAX_NB_JONCTIONS + 1];
 
 int existeChemin(int indice){
     visite[indice] = 1;
     if (indice == nbJonctions)
         return (1);
-    for (int i = MAX_NB_JONCTIONS ; i >= 1 ; i--){
+    for (int i = MAX_NB_JONCTIONS ; i >= 1 ; i--)
         if (jonctions[indice][i] > 0 && visite[i] == 0 && existeChemin(i)){
             chemin.push_back(make_pair(indice, i));
             return (1);
         }
-    }
     return (0);
 }
-
-vector<int> chemins[MAX_NB_JONCTIONS + 1];
-int     nbChemins = 0;
 
 int main(void){
     scanf("%d%d%d", &nbJonctions, &nbTuyaux, &nbBallades);
@@ -41,7 +36,6 @@ int main(void){
     while (existeChemin(1)){
         for (int i = 1 ; i <= MAX_NB_JONCTIONS ; i++)
             visite[i] = 0;
-        maxFlot++;
         chemins[nbChemins].push_back(chemin[0].second);
         for (unsigned int i = 0 ; i < chemin.size() ; i++){
             chemins[nbChemins].push_back(chemin[i].first);
