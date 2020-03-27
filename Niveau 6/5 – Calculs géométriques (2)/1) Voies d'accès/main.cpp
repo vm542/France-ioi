c3f73f86d61ad5e6318e611334b8e1c70380e2e7
maissouradze <nicolas.maissouradze@gmail.com>
2020-03-27 16:32:52 +0100

    Voies d'accès

--- Niveau 6/5 – Calculs géométriques (2)/1) Voies d'accès/main.cpp
+++ Niveau 6/5 – Calculs géométriques (2)/1) Voies d'accès/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cmath>
 
 using namespace std;
 
@@ -9,42 +10,37 @@ struct  Coord{
 Coord   nouv1, nouv2;
 int     nbSegments;
 
-float   coefficientDir(Coord point1, Coord point2){
-    return ((double)(point2.y - point1.y) / (double)(point2.x - point1.x));
+int orientation(Coord point1, Coord point2, Coord point3){
+    float   val = (float)((point2.y - point1.y) * (point3.x - point2.x) - (point3.y - point2.y) * (point2.x - point1.x));
+    if (val == 0)
+        return (0);
+    if (val > 0)
+        return (1);
+    return (2);
+}
+
+int     colinear(Coord point1, Coord point2, Coord point3, Coord point4){
+    if ((point1.x >= point3.x && point1.x <= point4.x) || (point2.x >= point3.x && point2.x <= point4.x))
+        return (1);
+    if ((point1.y >= point3.y && point1.y <= point4.y) || (point2.y >= point3.y && point2.y <= point4.y))
+        return (1);
+    return (0);
 }
 
 int     main(void){
-    scanf("%d%d%d%d", &nouv1.x, &nouv2.y, &nouv2.x, &nouv2.y);
-    printf("%lf ", coefficientDir(nouv1, nouv2));
+    scanf("%d%d%d%d", &nouv1.x, &nouv1.y, &nouv2.x, &nouv2.y);
     scanf("%d", &nbSegments);
     for (int i = 0 ; i < nbSegments ; i++){
         Coord   point1, point2;
         scanf("%d%d%d%d", &point1.x, &point1.y, &point2.x, &point2.y);
-        printf("%lf", coefficientDir(point1, point2));
-        //printf("%lf %lf\n", coefficientDir(point1, point2), coefficientDir(nouv1, nouv2));
-        /*if (coefficientDir(point1, point2) == coefficientDir(nouv1, nouv2)){
-            
-        }
+        int o1 = orientation(nouv1, nouv2, point1);
+        int o2 = orientation(nouv1, nouv2, point2);
+        int o3 = orientation(point1, point2, nouv1);
+        int o4 = orientation(point1, point2, nouv2);
+        if ((o1 != o2 && o3 != o4) || (o1 == 0 && o2 == 0 && o3 == 0 && o4 == 0 && colinear(nouv1, nouv2, point1, point2)))
+            printf("1\n");
         else
-            printf("%d\n", 0);*/
+            printf("0\n");
     }
     return (0);
 }
-
-/*
-
-
-25 28 0 40
-1
-10 3 3 18
-10 3 18 8
-18 8 13 21
-13 21 9 30
-9 30 15 38
-15 38 25 36
-13 21 25 36
-25 36 27 45
-27 45 32 38
-32 38 45 36
-
-*/
