b5be3c215b0346fff291c8f020f655a8fc774849
maissouradze <nicolas.maissouradze@gmail.com>
2020-03-31 00:42:20 +0200

    Amis zoinxiens

--- Niveau 6/1 – Graphes implicites (2)/2) Amis zoinxiens/main.cpp
+++ Niveau 6/1 – Graphes implicites (2)/2) Amis zoinxiens/main.cpp
@@ -1,74 +1,64 @@
 #include <stdio.h>
-#include <vector>
-#include <list>
 
-#define MAX_NB_ZOINXIENS 200001
+#define MAX_NB_ZOINXIENS 200000
 
 using namespace std;
 
-struct  Zoinxien{
-    int x, y, nbTentacules, age, visite = 0;
-    vector<int> amis;
-    Zoinxien(int x, int y, int nbTentacules, int age){
-        this->x = x;
-        this->y = y;
-        this->nbTentacules = nbTentacules;
-        this->age = age;
-    }
-    Zoinxien(){}
-};
-
-struct  Racine{
-    list<int>   enfants;
-};
-
-Zoinxien    zoinxiens[MAX_NB_ZOINXIENS + 1];
-Racine  racinesX[MAX_NB_ZOINXIENS + 1], racinesY[MAX_NB_ZOINXIENS + 1], racinesA[MAX_NB_ZOINXIENS + 1], racinesT[MAX_NB_ZOINXIENS + 1];
 
 int nbZoinxiens, nbAmis = 0;
+int racinesX[MAX_NB_ZOINXIENS + 1], racinesY[MAX_NB_ZOINXIENS + 1], racinesA[MAX_NB_ZOINXIENS + 1], racinesT[MAX_NB_ZOINXIENS + 1];
+int amities[MAX_NB_ZOINXIENS + 1];
+int tailles[MAX_NB_ZOINXIENS + 1];
 
-void parcourir(int indice){
-    zoinxiens[indice].visite = 1;
-    for (unsigned int i = 0 ; i < zoinxiens[indice].amis.size() ; i++){
-        if (zoinxiens[zoinxiens[indice].amis[i]].visite == 0){
-            nbAmis++;
-            parcourir(zoinxiens[indice].amis[i]);
-        }
+int racine(int iZonxien){
+    if (amities[iZonxien] != iZonxien)
+        amities[iZonxien] = racine(amities[iZonxien]);
+    return (amities[iZonxien]);
+}
+
+void    unir(int zA, int aB){
+    int zoinxienA = racine(amities[zA]);
+    int zoinxienB = racine(amities[aB]);
+    if (zoinxienA == zoinxienB)
+        return;
+    if (tailles[zoinxienB] < tailles[zoinxienA]){
+        amities[zoinxienB] = zoinxienA;
+        tailles[zoinxienA] += tailles[zoinxienB];
+    }
+    else{
+        amities[zoinxienA] = zoinxienB;
+        tailles[zoinxienB] += tailles[zoinxienA];
     }
 }
 
 int main(void){
     scanf("%d", &nbZoinxiens);
     for (int i = 1 ; i <= nbZoinxiens ; i++){
+        amities[i] = i;
+        tailles[i] = 1;
+    }
+    for (int i = 1 ; i <= nbZoinxiens ; i++){
         int x, y, nbTentacules, age;
         scanf("%d%d%d%d", &x, &y, &nbTentacules, &age);
-        zoinxiens[i] = Zoinxien(x, y, nbTentacules, age);
-        for (auto it = racinesX[x].enfants.begin(); it != racinesX[x].enfants.end(); ++it){
-            zoinxiens[i].amis.push_back(*it);
-            zoinxiens[*it].amis.push_back(i);
-        }
-        for (auto it = racinesY[y].enfants.begin(); it != racinesY[y].enfants.end(); ++it){
-            zoinxiens[i].amis.push_back(*it);
-            zoinxiens[*it].amis.push_back(i);
-        }
-        for (auto it = racinesT[nbTentacules].enfants.begin(); it != racinesT[nbTentacules].enfants.end(); ++it){
-            zoinxiens[i].amis.push_back(*it);
-            zoinxiens[*it].amis.push_back(i);
-        }
-        racinesX[x].enfants.push_back(i);
-        racinesY[y].enfants.push_back(i);
-        racinesT[nbTentacules].enfants.push_back(i);
-        for (int j = age - 5 ; j <= age + 5 ; j++){
-            if (j >= 0 && j <= 200000){
-                for (auto it = racinesA[j].enfants.begin(); it != racinesA[j].enfants.end(); ++it){
-                    zoinxiens[i].amis.push_back(*it);
-                    zoinxiens[*it].amis.push_back(i);
-                }
-            }
-        }
-        racinesA[age].enfants.push_back(i);
+        if (racinesX[x] == 0)
+            racinesX[x] = i;
+        unir(racinesX[x], i);
+        if (racinesY[y] == 0)
+            racinesY[y] = amities[i];
+        unir(racinesY[y], i);
+        if (racinesT[nbTentacules] == 0)
+            racinesT[nbTentacules] = amities[i];            
+        unir(racinesT[nbTentacules], amities[i]);
+        if (racinesA[age] == 0)
+            racinesA[age] = i;
+        for (int j = age - 5 ; j <= age + 5 ; j++)
+            if (j >= 0 && j <= MAX_NB_ZOINXIENS)
+                if (racinesA[j] != 0)
+                    unir(racinesA[j], i);
     }
-    parcourir(1);
+    for (int i = 2 ; i <= nbZoinxiens ; i++)
+        if (racine(i) == racine(1))
+            nbAmis++;
     printf("%d", nbAmis);
     return (0);
 }
